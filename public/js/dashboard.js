7d908532a9f5d6dd4550ad85ebe7457e3a132656
Tim Chang <b00501007@gmail.com>
2020-07-23 16:53:01 +0800

    Add create and join room feature to dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -32,8 +32,18 @@ const View = {
         </div>
       `;
     }
-    console.log(rooms);
+
     get('.dashboard-rooms').innerHTML = htmlContent;
+  },
+  closeFormContainer: function () {
+    get('.form-container').classList.add('hide');
+    get('.form-container form:not(.hide)').classList.add('hide');
+
+    const inputs = getAll('form input');
+    for (let inputIndex = 0; inputIndex < inputs.length; inputIndex++) {
+      const input = inputs[inputIndex];
+      input.value = '';
+    }
   },
 };
 
@@ -41,6 +51,41 @@ const Controller = {
   init: async function () {
     await this.getRoom();
     View.renderRoom();
+  },
+  initListener: function () {
+    // sign out
+    get('.dashboard-navbar .signout-btn').addEventListener('click', (e) => {
+      localStorage.removeItem('access_JWT');
+      localStorage.removeItem('user');
+      Swal.fire({
+        icon: 'success',
+        title: 'sign out successfully',
+      }).then(async (result) => {
+        localStorage.removeItem('access_JWT');
+        localStorage.removeItem('user');
+        location.href = '/';
+      });
+    });
+
+    // create room
+    get('.create-join-room .create-btn').addEventListener('click', (e) => {
+      get('.form-container').classList.remove('hide');
+      get('.create-form').classList.remove('hide');
+    });
+
+    // join room
+    get('.create-join-room .join-btn').addEventListener('click', (e) => {
+      get('.form-container').classList.remove('hide');
+      get('.join-form').classList.remove('hide');
+    });
+
+    // form container
+    get('.form-container').addEventListener('mousedown', (e) => {
+      if (e.target.closest('form')) {
+        return;
+      }
+      View.closeFormContainer();
+    });
   },
   getRoom: async function () {
     const url = HOMEPAGE_URL + '/roomUser';
@@ -73,6 +118,86 @@ const Controller = {
 
     Model.rooms = rooms;
   },
+  createRoom: function () {
+    const password = get('.create-form input[name="createRoomPassword"]').value;
+    if (!password) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Please setting room password',
+      });
+      return;
+    }
+
+    // create room
+    const url = HOMEPAGE_URL + '/room';
+    const access_JWT = localStorage.getItem('access_JWT');
+
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({ password }),
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${access_JWT}`,
+      },
+    }).then(res => res.json())
+      .then(resObj => {
+        const { authError, error, room } = resObj;
+        if (error) {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please sign in first',
+          }).then(async (result) => {
+            localStorage.removeItem('access_JWT');
+            localStorage.removeItem('user');
+            location.href = '/';
+          });
+        } else {
+          location.href = `/room.html?room=${room}`;
+        }
+      })
+      .catch(error => console.log(error));
+  },
+  joinRoom: function () {
+    const room = get('.join-form input[name="room"]').value;
+    const password = get('.join-form input[name="joinRoomPassword"]').value;
+    if (!room || !password) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'All fields are required',
+      });
+      return;
+    }
+
+    // join room, create roomUser
+    const url = HOMEPAGE_URL + '/roomUser';
+    const access_JWT = localStorage.getItem('access_JWT');
+
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({ room, password }),
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${access_JWT}`,
+      },
+    }).then(res => res.json())
+      .then(resObj => {
+        const { authError, error, message } = resObj;
+        if (error) {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: error,
+          });
+        } else {
+          location.href = `/room.html?room=${room}`;
+        }
+      })
+      .catch(error => console.log(error));
+  },
 };
 
 Controller.init();
+Controller.initListener();
