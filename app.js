e0562b89e05acc647e7add92b8738f6359f1d433
Tim Chang <b00501007@gmail.com>
2020-07-25 14:23:27 +0800

    Add method patch to CORS

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Methods', 'POST, GET');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PATCH');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   }
   next();
