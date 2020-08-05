c7ad242192eb9742492eeeef943ce45287c2a881
Tim Chang <b00501007@gmail.com>
2020-08-05 14:41:08 +0800

    Add FK whiteboard_id to draw

--- server/models/draw_model.js
+++ server/models/draw_model.js
@@ -1,6 +1,17 @@
 const { query, transaction, commit, rollback } = require('../../util/mysqlcon');
 
-const createDraw = async (draw) => {
+const createDraw = async (_draw) => {
+  const { room_id, start_at, link, } = _draw;
+  const whiteboards = await query('SELECT id FROM whiteboard WHERE room_id = ? AND start_at = ? ', [room_id, start_at]);
+  const whiteboard = whiteboards[0];
+  if (!whiteboard) {
+    return { error: 'Whiteboard does not exist.' };
+  }
+  const draw = {
+    whiteboard_id: whiteboard.id,
+    link,
+  };
+
   try {
     await transaction();
     await query('INSERT INTO draw SET ?', draw);
@@ -17,8 +28,9 @@ const getDraw = async (requirement) => {
   const condition = { query: '', sql: '', binding: [] };
 
   if (requirement.room_id && requirement.start_at) {
-    condition.query = 'SELECT link FROM draw ';
-    condition.sql = 'WHERE room_id = ? AND start_at = ? ORDER BY id ASC';
+    condition.query = 'SELECT draw.link FROM draw ';
+    condition.sql = 'INNER JOIN whiteboard ON whiteboard.id = draw.whiteboard_id ';
+    condition.sql += 'WHERE whiteboard.room_id = ? AND whiteboard.start_at = ? ORDER BY draw.id ASC';
     condition.binding = [requirement.room_id, requirement.start_at];
   } else {
     return { error: 'requirement is necessary' };
