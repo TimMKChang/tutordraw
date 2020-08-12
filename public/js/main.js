ecc04d412f36718750cc5859ce1ba8eb42551e01
Tim Chang <b00501007@gmail.com>
2020-08-12 23:50:39 +0800

    Remove header, footer and related functions

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,3 @@
-main();
-
-async function main() {
-  await loadHaeder();
-  await loadFooter();
-}
-
-async function loadHaeder() {
-  await new Promise((resolve, reject) => {
-    $("#header").load("/header.html", () => {
-      resolve();
-    });
-  });
-}
-
-async function loadFooter() {
-  await new Promise((resolve, reject) => {
-    $("#footer").load("/footer.html", () => {
-      resolve();
-    });
-  });
-}
-
 function get(selector) {
   return document.querySelector(selector);
 }
